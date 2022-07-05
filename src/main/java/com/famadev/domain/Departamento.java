9a4d94934be609c037836fabf4162351d815ddb3
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-05 07:57:43 -0300

    Mapeamento da entidade cargo

--- src/main/java/com/famadev/domain/Departamento.java
+++ src/main/java/com/famadev/domain/Departamento.java
@@ -2,7 +2,9 @@ package com.famadev.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "DEPARTAMENTOS")
@@ -10,11 +12,22 @@ public class Departamento extends AbstractEntity<Long>{
     @Column(name = "nome", nullable = false, unique = true, length = 60)
     private String nome;
 
+    // Muitos cargos para um departamento
+    // mappedBy - definindo quemee o um da relacao ou lado forte
+    @OneToMany(mappedBy = "departamento")
+    private List<Cargo> cargos;
+
     //Getters ee setters
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    public List<Cargo> getCargos() {
+        return cargos;
+    }
+    public void setCargos(List<Cargo> cargos) {
+        this.cargos = cargos;
     }
 }
