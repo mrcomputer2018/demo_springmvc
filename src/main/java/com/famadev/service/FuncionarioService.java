c5d8515e52f3f0f7ee35a3f4744c462eb3a508fc
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-08 07:54:23 -0300

    Criando service Funcionario

---
+++ src/main/java/com/famadev/service/FuncionarioService.java
@@ -0,0 +1,17 @@
+package com.famadev.service;
+
+import com.famadev.domain.Funcionario;
+
+import java.util.List;
+
+public interface FuncionarioService {
+    void salvar(Funcionario funcionario);
+
+    void editar(Funcionario funcionario);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
+}
---
+++ src/main/java/com/famadev/service/FuncionarioServiceImpl.java
@@ -0,0 +1,44 @@
+package com.famadev.service;
+
+import com.famadev.dao.FuncionarioDao;
+import com.famadev.domain.Funcionario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = false)
+public class FuncionarioServiceImpl implements FuncionarioService{
+
+    @Autowired
+    private FuncionarioDao dao;
+
+    @Override
+    public void salvar(Funcionario funcionario) {
+        dao.save(funcionario);
+    }
+
+    @Override
+    public void editar(Funcionario funcionario) {
+        dao.update(funcionario);
+    }
+
+    @Override
+    public void excluir(Long id) {
+        dao.delete(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Funcionario buscarPorId(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Funcionario> buscarTodos() {
+        return dao.findAll();
+    }
+}
