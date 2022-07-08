457356d0acc1c729bda06a23afd2d550dd394f63
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-08 07:34:45 -0300

    Criando service cargo

---
+++ src/main/java/com/famadev/service/CargoService.java
@@ -0,0 +1,18 @@
+package com.famadev.service;
+
+import com.famadev.domain.Cargo;
+
+import java.util.List;
+
+public interface CargoService {
+
+    void salvar(Cargo cargo);
+
+    void editar(Cargo cargo);
+
+    void excluir(Long id);
+
+    Cargo buscarPorId(Long id);
+
+    List<Cargo> buscarTodos();
+}
---
+++ src/main/java/com/famadev/service/CargoServiceImpl.java
@@ -0,0 +1,45 @@
+package com.famadev.service;
+
+import com.famadev.dao.CargoDao;
+import com.famadev.domain.Cargo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = false)
+public class CargoServiceImpl implements CargoService{
+    // Injetando dependencia
+    @Autowired
+    private CargoDao dao;
+
+    // Metodos
+    @Override
+    public void salvar(Cargo cargo) {
+        dao.save(cargo);
+    }
+
+    @Override
+    public void editar(Cargo cargo) {
+        dao.update(cargo);
+    }
+
+    @Override
+    public void excluir(Long id) {
+        dao.delete(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cargo buscarPorId(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cargo> buscarTodos() {
+        return dao.findAll();
+    }
+}
