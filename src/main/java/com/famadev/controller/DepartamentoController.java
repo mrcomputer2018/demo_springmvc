74b03658a6bff11f5dd6184d437918b0fdb86fa6
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-18 17:57:25 -0300

    editar departamentos

--- src/main/java/com/famadev/controller/DepartamentoController.java
+++ src/main/java/com/famadev/controller/DepartamentoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +34,18 @@ public class DepartamentoController {
     public String salvar(Departamento departamento) {
         service.salvar(departamento);
         // retorno para pagina de cadastro
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("departamento", service.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento departamento) {
+        service.editar(departamento);
         return "redirect:/departamentos/cadastrar";
     }
 }
