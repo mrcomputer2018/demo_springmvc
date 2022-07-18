8e988c148baa0bf00037791ffa6923205dfc82e2
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-18 16:43:23 -0300

    salvando cadastro de departamentos

--- src/main/java/com/famadev/controller/DepartamentoController.java
+++ src/main/java/com/famadev/controller/DepartamentoController.java
@@ -1,20 +1,35 @@
 package com.famadev.controller;
 
+import com.famadev.domain.Departamento;
+import com.famadev.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+    // atributo
+    // Para poder acessar oos metodos de service
+    @Autowired
+    private DepartamentoService service;
 
     @GetMapping("/cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Departamento departamento) {
         return "/departamento/cadastro";
     }
 
     @GetMapping("/listar")
     public String listar(){
         return"/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento) {
+        service.salvar(departamento);
+        // retorno para pagina de cadastro
+        return "redirect:/departamentos/cadastrar";
     }
 }
