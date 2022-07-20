546fba80c806950b1f6679321faa457e3cc8992e
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-20 17:45:28 -0300

    pagina departamentos menssagens de alerta

--- src/main/java/com/famadev/controller/DepartamentoController.java
+++ src/main/java/com/famadev/controller/DepartamentoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/departamentos")
@@ -31,8 +32,9 @@ public class DepartamentoController {
     }
 
     @PostMapping("/salvar")
-    public String salvar(Departamento departamento) {
+    public String salvar(Departamento departamento, RedirectAttributes attr) {
         service.salvar(departamento);
+        attr.addFlashAttribute("success", "Departamento salvo com sucesso.");
         // retorno para pagina de cadastro
         return "redirect:/departamentos/cadastrar";
     }
@@ -44,16 +46,20 @@ public class DepartamentoController {
     }
 
     @PostMapping("/editar")
-    public String editar(Departamento departamento) {
+    public String editar(Departamento departamento, RedirectAttributes attr) {
         service.editar(departamento);
+        attr.addFlashAttribute("success", "Departamento editado com sucesso.");
         return "redirect:/departamentos/cadastrar";
     }
 
     @GetMapping("/excluir/{id}")
     public String excluir(@PathVariable("id") Long id, ModelMap model) {
         // Se nao tem cargos eh feita a exclusão
-        if (!service.departamentosTemCargos(id)) {
+        if (service.departamentosTemCargos(id)) {
+            model.addAttribute("fail", "Departamento não foi removido. A cargo(s) vinculados");
+        } else {
             service.excluir(id);
+            model.addAttribute("success", "Departamento removido.");
         }
         // Retorno chamando o metodo listar
         // pode fazer um redirect para /departamentos/listar
