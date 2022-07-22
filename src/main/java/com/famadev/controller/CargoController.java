61bf7645eb6069847745f573d6616298ee543871
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-22 10:05:35 -0300

    CargoController criando o metodo salvar

--- src/main/java/com/famadev/controller/CargoController.java
+++ src/main/java/com/famadev/controller/CargoController.java
@@ -1,20 +1,51 @@
 package com.famadev.controller;
 
+import com.famadev.domain.Cargo;
+import com.famadev.domain.Departamento;
+import com.famadev.service.CargoService;
+import com.famadev.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
+    // injecao de dados
+    @Autowired
+    private CargoService cargoService;
 
+    @Autowired
+    private DepartamentoService departamentoService;
+
+    // metodos
     @GetMapping("/cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Cargo cargo) {
         return "/cargo/cadastro";
     }
 
     @GetMapping("/listar")
     public String listar() {
         return "/cargo/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Cargo cargo, RedirectAttributes attr) {
+        cargoService.salvar(cargo);
+        attr.addFlashAttribute("success", "Cargo salvo com sucesso.");
+        // retorno para pagina de cadastro
+        return "redirect:/cargos/cadastrar";
+    }
+
+    // enviando lista de departamentos para pagina de cadastro
+    @ModelAttribute("departamentos")
+    public List<Departamento> listaDeDepartamentos(){
+        return departamentoService.buscarTodos();
     }
 }
