0df72ec49420b6be566f0481373fe834a30849a9
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-23 08:20:33 -0300

    criando classe de conversao em objeto

--- src/main/java/com/famadev/conversor/StringToDepartamentoConverter.java
+++ src/main/java/com/famadev/conversor/StringToDepartamentoConverter.java
@@ -7,7 +7,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class StringToDepartamentoConverter  implements Converter<String, Departamento> {
+public class StringToDepartamentoConverter implements Converter<String, Departamento> {
     @Autowired
     private DepartamentoService service;
 
