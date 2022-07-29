046a9b29b2f8003d40e1f13251d8f4818fea475b
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-29 07:54:19 -0300

    conversor de cargos

--- src/main/java/com/famadev/conversor/StringToCargoConversor.java
+++ src/main/java/com/famadev/conversor/StringToCargoConversor.java
@@ -4,7 +4,9 @@ import com.famadev.domain.Cargo;
 import com.famadev.service.CargoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StringToCargoConversor implements Converter<String, Cargo> {
 
     @Autowired
