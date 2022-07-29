0ee3e0665883a91cc17c0656b441c86a9974ffba
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-07-29 09:05:54 -0300

    anotacoes para localizacao brasil

--- src/main/java/com/famadev/DemoMvcApplication.java
+++ src/main/java/com/famadev/DemoMvcApplication.java
@@ -2,12 +2,21 @@ package com.famadev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class DemoMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoMvcApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(DemoMvcApplication.class, args);	}
+
+	// Setando para portugues brasil
+	@Bean
+	public LocaleResolver lccaleResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 
 }
