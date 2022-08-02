81a2b7bd44c9e39fdaecd73904127e0a5694693d
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-08-02 10:17:08 -0300

    habilitando pesquisa por nome em dao de funcionarios

--- src/main/java/com/famadev/dao/FuncionarioDaoImpl.java
+++ src/main/java/com/famadev/dao/FuncionarioDaoImpl.java
@@ -10,9 +10,12 @@ import java.util.List;
 public class FuncionarioDaoImpl extends AbstractDao<Funcionario, Long> implements FuncionarioDao{
     @Override
     public List<Funcionario> findByNome(String nome) {
-        TypedQuery<Funcionario> query = getEntityManager()
-                .createQuery("select f from Funcionario f where f.nome like:nome", Funcionario.class);
-        query.setParameter("nome", nome);
-        return query.getResultList();
+        // Usando o JPQL
+        //TypedQuery<Funcionario> query = getEntityManager()
+                //.createQuery("select f from Funcionario f where f.nome like:nome", Funcionario.class);
+        //query.setParameter("nome", nome);
+        //return query.getResultList();
+
+        return createQuery("select f from Funcionario f where f.nome like concat('%', ?1, '%')", nome);
     }
 }
