af828e557f604350a96348580f6f271a0a68c903
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-08-02 11:12:44 -0300

    habilitando pesquisa por datas em service de funcionarios

--- src/main/java/com/famadev/service/FuncionarioServiceImpl.java
+++ src/main/java/com/famadev/service/FuncionarioServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -51,5 +53,18 @@ public class FuncionarioServiceImpl implements FuncionarioService{
     @Override
     public List<Funcionario> buscarPorCargo(Long id) {
         return dao.findByCargoId(id);
+    }
+
+    @Override
+    public List<Funcionario> buscarPorDatas(LocalDate entrada, LocalDate saida) {
+        if (entrada != null && saida != null) {
+            return dao.findByDataEntradaDataSaida(entrada, saida);
+        } else if (entrada != null) {
+            return dao.findByDataEntrada(entrada);
+        } else if (saida != null) {
+            return dao.findByDataSaida(saida);
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
