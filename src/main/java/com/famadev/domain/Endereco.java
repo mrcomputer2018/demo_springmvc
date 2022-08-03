5d4214a40161745983cec2e8ed97268095bf0fcc
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-08-03 08:33:48 -0300

    habilitando validacao backend nos dominios de funcionarios e enderecos

--- src/main/java/com/famadev/domain/Endereco.java
+++ src/main/java/com/famadev/domain/Endereco.java
@@ -1,28 +1,43 @@
 package com.famadev.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "ENDERECOS")
 public class Endereco  extends AbstractEntity<Long>{
     // atributos
+    @NotBlank
+    @Size(min = 3, max = 255)
     @Column(name = "logradouro", nullable = false)
     private String logradouro;
 
+    @NotBlank
+    @Size(min = 3, max = 255)
     @Column(name = "bairro", nullable = false)
     private String bairro;
 
+    @NotBlank
+    @Size(min = 3, max = 255)
     @Column(name = "cidade", nullable = false)
     private String cidade;
 
+    @NotNull(message = "{NotNull.endereco.uf}")
     @Column(name = "uf", nullable = false, length = 2)
     @Enumerated(EnumType.STRING)
     private UF uf;
 
+    @NotBlank
+    @Size(min = 9, max = 9, message = "{Size.endereco.cep}")
     @Column(name = "cep", nullable = false, length = 9)
     private String cep;
 
+    @NotNull(message = "{otNull.endereco.numero}")
+    @Digits(integer = 5, fraction = 0)
     @Column(name = "numero", nullable = false, length = 5)
     private Integer numero;
 
--- src/main/java/com/famadev/domain/Funcionario.java
+++ src/main/java/com/famadev/domain/Funcionario.java
@@ -5,6 +5,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -13,15 +18,21 @@ import java.time.LocalDate;
 @Table(name = "FUNCIONARIOS")
 public class Funcionario extends AbstractEntity<Long>{
     // atributos
+    // anatacoes sem message receberam a anatocao padrao
+    @NotBlank
+    @Size(min = 3, max = 255)
     @Column(name = "nome", nullable = false, unique = true)
     private String nome;
 
     // columnDefinition - Define o tipo de dado que vamos ter no banco de dados
     // @NumberFormat - para converter oo dado salario
+    @NotNull
     @NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
     @Column(name = "salario", nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
     private BigDecimal salario;
 
+    @NotNull
+    @PastOrPresent(message = "{PastOrPresent.funcionario.dataEntrada}") // recebe o message de validationMessages
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
     private LocalDate dataEntrada;
@@ -34,10 +45,13 @@ public class Funcionario extends AbstractEntity<Long>{
     private LocalDate dataSaida;
 
     // cascade - inserir por cascata o endereco
+    // Valid - habilita a validacao usando as config dee validacao da classe endereco
+    @Valid
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id_fk")
     private Endereco endereco;
 
+    @NotNull(message = "{NotNull.funcionario.cargo}")
     @ManyToOne
     @JoinColumn(name = "cargo_id_fk")
     private Cargo cargo;
