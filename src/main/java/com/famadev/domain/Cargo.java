c8f9c5bc0a806d2b8a6a22bde0356a3c4d873172
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-08-03 07:19:21 -0300

    habilitando validacao backend nos dominios

--- src/main/java/com/famadev/domain/Cargo.java
+++ src/main/java/com/famadev/domain/Cargo.java
@@ -1,15 +1,21 @@
 package com.famadev.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
 @Table(name = "CARGOS")
 public class Cargo extends AbstractEntity<Long>{
     // atrbutos
+    @NotBlank(message = "O nome do cargo é obrigatorio")
+    @Size(max = 60, message = "O nome do cargo deve conter no máximo de {max} caracteres.")
     @Column(name = "nome",nullable = false, unique = true, length = 60)
     private String nome;
 
+    @NotNull(message = "Selecione o departamento relativo ao cargo")
     @ManyToOne
     @JoinColumn(name = "id_departamento_fk")
     private Departamento departamento;
